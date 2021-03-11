69d65e09e2563a9183ab1966969173a2e08dd40f
nangelva <noora.angelva@edu.lapinamk.fi>
2021-03-11 12:26:51 +0200

    PikkupisteetQuiz

--- LiikkuvaKoulu1_1/Assets/Scripts/Pathfollower.cs
+++ LiikkuvaKoulu1_1/Assets/Scripts/Pathfollower.cs
@@ -8,18 +8,30 @@ public class Pathfollower : MonoBehaviour
     public GameObject sticker;
 
     Sticker koodi;
+    QuizManager kyssari;
     float Timer;
     static Vector3 CurrentPositionHolder;
     int CurrentNode;
     int x;
+    int y;
+    int z;
+    int[] pisteet = {10, 76, 171, 247, 297, 382, 458, 538, 614, 644, 754, 830, 880, 980};
 
     void Start()
     {
+        kyssari = GameObject.Find("QuizManager").GetComponent<QuizManager>();
         sticker = GameObject.Find("Sticker");
         koodi = sticker.GetComponent<Sticker>();
         PathNode = GetComponentsInChildren<Node>();
         CheckNode();
         x = 0;
+        y = 2;// haettu tietokannasta käydyt pisteet - kesken
+
+        for(z = 0; z < y; z++)
+        {
+           // pisteet.RemoveAt(y);
+        }
+        
     }
 
     void CheckNode()
@@ -32,6 +44,12 @@ public class Pathfollower : MonoBehaviour
     {
         if (x < koodi.matka)
         {
+            if(IndexOf(pisteet, x))
+            {
+                kyssari.generateQuestion();
+                Time.timescale = 0;
+            }
+
             //Timer += Time.deltaTime * 0.00771f;
             Timer += 0.5f;
             if(sticker.transform.position != CurrentPositionHolder)
--- LiikkuvaKoulu1_1/Assets/Scripts/QuizManager.cs
+++ LiikkuvaKoulu1_1/Assets/Scripts/QuizManager.cs
@@ -9,27 +9,28 @@ public class QuizManager : MonoBehaviour
     public List<QuestionsAndAnswers> QnA; 
     public GameObject[] options;
     public int currentQuestion;
-
+    string[] vastaukset;
     public Text QuestionTxt;
+    GetHaku haku;
 
 
     private void Start()
     {
-        generateQuestion();
+        haku = GameObject.Find("Lahettaja").GetComponent<GetHaku>();
     }
 
     public void correct()
     {
         QnA.RemoveAt(currentQuestion);
-        generateQuestion();
+       // generateQuestion();
     }
 
     void SetAnswers()
     {
-        for (int i = 0; i < options.Length; i++)
+        for (int i = 0; i < vastaukset.Length; i++)
         {
             options[i].GetComponent<AnswerScript>().isCorrect = false;
-            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
+            options[i].transform.GetChild(0).GetComponent<Text>().text = vastaukset[i];
 
             if(QnA[currentQuestion].CorrectAnswer == i+1)
             {
@@ -38,15 +39,19 @@ public class QuizManager : MonoBehaviour
         }
     }
 
-
     void generateQuestion()
     {
-        currentQuestion = Random.Range(0, QnA.Count);
+       // currentQuestion = Random.Range(0, QnA.Count);
+        value[0] = (string)kt.text;
+        value[1] = (string)ss.text;
+        haku.data = value;
+        haku.id = 1;
 
+        haku.StartCoroutine("LocationHandler");
+        currentQuestion = "";
+        vastaukset ="";
 
-        QuestionTxt.text = QnA[ currentQuestion].Question;
+        QuestionTxt.text = QnA[currentQuestion].Question;
         SetAnswers();
-
-
     }
 }
